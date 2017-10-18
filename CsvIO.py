e82a79691c5746b242df34e747fc20ab87ca9788
Vandana Jain <vandanaj@hotmail.com>
2017-10-18 16:27:01 -0400

    Improving PipeLineTests

--- CsvIO.py
+++ CsvIO.py
@@ -1,6 +1,7 @@
 """module to define CsvIO"""
 import csv
 
+
 def read_stock_csv(fname):
     """read_stock_csv reads csv file fname and returns an array of OrderedDict"""
     stocks = []
@@ -10,10 +11,11 @@ def read_stock_csv(fname):
             stocks.append(row)
         return stocks
 
+
 def write_stock_csv(fname, items):
     """write_stock_csv creates csv file with fname and writes items to it"""
     with open(fname, 'w') as csv_file:
-        if len(items)>0:
+        if len(items) > 0:
             writer = csv.DictWriter(
                 csv_file, lineterminator='\n', fieldnames=items[0].keys())
             writer.writeheader()
--- TestPipeLine.py
+++ TestPipeLine.py
@@ -1,8 +1,7 @@
 '''test class'''
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock, call
 import collections
-
 from PipeLine import run
 
 
@@ -14,36 +13,45 @@ class TestPipeLine(unittest.TestCase):
     @patch('PipeLine.write_stock_csv')
     @patch('PipeLine.read_stock_csv')
     def test_run(self, mock_readcsv, mock_writecsv, mock_convert_prices, mock_convert_prices_an):
+        '''tests run method for pound conversion'''
+        mock_parent = Mock()
+        mock_parent.attach_mock(mock_readcsv, 'a')
+        mock_parent.attach_mock(mock_writecsv, 'b')
+        mock_parent.attach_mock(mock_convert_prices, 'c')
+        mock_parent.attach_mock(mock_convert_prices_an, 'd')
         obj1 = collections.OrderedDict(
             [('a', 'A1'), ('b', 'B1'), ('c', 'C1')])
         obj2 = collections.OrderedDict(
-            [('a', 'A1'), ('b', 'B1'), ('c', 'C1')])
+            [('a', 'A2'), ('b', 'B2'), ('c', 'C2')])
         objects1 = [obj1, obj2]
         mock_readcsv.return_value = objects1
         run('source', 'dest', 'pound')
-        mock_readcsv.assert_called_with('source')
-        mock_convert_prices.assert_called_with(obj1, 0.7578)
-        mock_convert_prices.assert_called_with(obj2, 0.7578)
-        mock_writecsv.assert_called_with('dest', objects1)
         mock_convert_prices_an.assert_not_called()
+        mock_parent.assert_has_calls([call.a('source'), call.c(
+            obj1, 0.7578), call.c(obj2, 0.7578), call.b('dest', objects1)])
 
     @patch('PipeLine.convert_prices_an_roman')
     @patch('PipeLine.convert_prices')
     @patch('PipeLine.write_stock_csv')
     @patch('PipeLine.read_stock_csv')
     def test_run_an(self, mock_readcsv, mock_writecsv, mock_convert_prices, mock_convert_prices_an):
+        '''tests run method for roman conversion'''
+        mock_parent = Mock()
+        mock_parent.attach_mock(mock_readcsv, 'a')
+        mock_parent.attach_mock(mock_writecsv, 'b')
+        mock_parent.attach_mock(mock_convert_prices, 'c')
+        mock_parent.attach_mock(mock_convert_prices_an, 'd')
         obj1 = collections.OrderedDict(
             [('a', 'A1'), ('b', 'B1'), ('c', 'C1')])
         obj2 = collections.OrderedDict(
-            [('a', 'A1'), ('b', 'B1'), ('c', 'C1')])
+            [('a', 'A2'), ('b', 'B2'), ('c', 'C2')])
         objects1 = [obj1, obj2]
         mock_readcsv.return_value = objects1
         run('source', 'dest', 'roman')
-        mock_readcsv.assert_called_with('source')
-        mock_convert_prices_an.assert_called_with(obj1)
-        mock_convert_prices_an.assert_called_with(obj2)
-        mock_writecsv.assert_called_with('dest', objects1)
         mock_convert_prices.assert_not_called()
+        mock_parent.assert_has_calls([call.a('source'), call.d(
+            obj1), call.d(obj2), call.b('dest', objects1)])
+
 
 if __name__ == '__main__':
     unittest.main(exit=False)
